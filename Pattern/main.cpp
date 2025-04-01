0f603bc25d1b152371fb8d115580ded018af28c2
varchunn <varvaram186@gmail.com>
2025-04-01 21:06:28 +0300

    Initial Commit

---
+++ Pattern/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Pattern
+//
+//  Created by Варя Мінєнкова on 01.04.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
